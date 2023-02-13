9891a1e71c2cb3046c4f0715c56fdf2f935f4369
Samudra Perera <samudrapup@gmail.com>
2023-02-13 15:46:23 -0500

    Moving profile component into the profile tab on the dashboard

--- client/src/pages/UserInformation.jsx
+++ client/src/pages/UserInformation.jsx
@@ -1,7 +1,11 @@
 import { Tab, TabList, TabPanel, TabPanels, Tabs, Text } from "@chakra-ui/react";
 import React from "react";
+import Profile from "../components/Profile";
+import { useOutletContext } from "react-router-dom";
 
 const UserInformation = () => {
+    const userID = useOutletContext()
+    console.log(userID)
   return (
     <Tabs mt="40px" p="20px" colorScheme="purple" variant="enclosed">
       <TabList>
@@ -14,7 +18,7 @@ const UserInformation = () => {
       </TabList>
       <TabPanels>
         <TabPanel>
-          <Text>1</Text>
+          <Profile userID={userID}/>
         </TabPanel>
         <TabPanel>
           <Text>2</Text>
