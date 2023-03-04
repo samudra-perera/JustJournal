33486dbedec82b5506a00f70b7dff4542424629d
Samudra Perera <samudrapup@gmail.com>
2023-03-04 00:16:47 -0500

    Added the follower card to the search results

--- client/src/components/SearchResults.jsx
+++ client/src/components/SearchResults.jsx
@@ -2,14 +2,14 @@ import React, { useState } from "react";
 import { useLocation, useOutletContext } from "react-router-dom";
 import axios from "axios";
 import { useEffect } from "react";
-import { Heading, SimpleGrid, Text } from "@chakra-ui/react";
+import { Heading, SimpleGrid } from "@chakra-ui/react";
 import SearchJournalCard from "./SearchJournalCard";
-
+import FollowerCard from "./FollowerCard";
 
 const SearchResults = () => {
   //State to store the response from the server
-  const [profiles, setProfiles] = useState([])
-  const [journals, setJournals] = useState([])
+  const [profiles, setProfiles] = useState([]);
+  const [journals, setJournals] = useState([]);
   //useLocation is used to take in the date sent from the searchbar via useNavigate()
   const { state } = useLocation();
 
@@ -27,33 +27,45 @@ const SearchResults = () => {
             withCredentials: true,
           }
         );
-        setProfiles(res.data.profiles)
-        setJournals(res.data.journals)
+        setProfiles(res.data.profiles);
+        setJournals(res.data.journals);
       } catch (err) {
         console.log(err);
       }
     };
     getSearch();
   }, []);
+  console.log(profiles[0])
   return (
     <>
-        <Heading>Journals</Heading>
-        {journals.length === 0 ? <p>Could not find any journals with that search</p> : 
-        (
-            <SimpleGrid minChildWidth="250px" spacing={10}>
-                {
-                    journals.map(journal => {
-                        return (
-                            <SearchJournalCard title={journal.title} id={journal._id}/>
-                        )
-                    })
-                }
-            </SimpleGrid>
-        )}
-        <Heading>Users</Heading>
-        {profiles.length === 0 ? <p>Could not find any users with that username</p> : <p>Found ones</p>}
+      <Heading>Journals</Heading>
+      {journals.length === 0 ? (
+        <p>Could not find any journals with that search</p>
+      ) : (
+        <SimpleGrid minChildWidth="250px" spacing={10}>
+          {journals.map((journal) => {
+            return (
+              <SearchJournalCard
+                title={journal.title}
+                id={journal._id}
+                posPromptOne={journal.posPromptOne}
+              />
+            );
+          })}
+        </SimpleGrid>
+      )}
+      <Heading>Users</Heading>
+      {profiles.length === 0 ? (
+        <p>Could not find any users with that username</p>
+      ) : (
+        <SimpleGrid minChildWidth="250px" spacing={10}>
+          {profiles[0].map((follower) => {
+            return <FollowerCard user={follower.user}/>;
+          })}
+        </SimpleGrid>
+      )}
     </>
-  )
+  );
 };
 
 export default SearchResults;
